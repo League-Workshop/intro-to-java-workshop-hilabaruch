95ce8e4f6f67cbd8faa4d1d501901ba0badabbc9
League Student <league@wtsim17.attlocal.net>
2019-06-19 14:32:29 -0700

    something

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -9,22 +9,26 @@ public class MadlibsOfTheAmazon {
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
 		
 		// Get the user to enter an adjective
-
+		String name =JOptionPane.showInputDialog(null,"enter an adjective");
 		// Get the user to enter a type of liquid
-
+		String bob =JOptionPane.showInputDialog(null,"enter a type of liquid");
 		// Get the user to enter a body part
-
+		String rose =JOptionPane.showInputDialog(null,"enter a body part");
 		// Get the user to enter a verb
-
+		String blue =JOptionPane.showInputDialog(null,"enter a verb");
 		// Get the user to enter a place
-
+		String red =JOptionPane.showInputDialog(null,"enter a place");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
 		// likely take a bite out of your [body part] if you [verb]. Whatever
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!
-		
+		JOptionPane.showInputDialog(null,"Piranhas are more "+name+" during the day, so cross the river at"
+		+" night .Piranhas are attracted to fresh " +bob+" and will most"
+		+" likely take a bite out of your " +rose+  "if you" +blue+". Whatever"
+		+" you do, if you have an open wound, try to find another way to get"
+		+"back to the"  +red+". Good luck!");
 		// Make a pop-up for the final story. You can use \n to go to the next line
 		
 
